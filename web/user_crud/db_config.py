b45b5de94d6765ab18f3ba9ca2410d3af2ffa927
Dilshan Hangawaththa <dilshan.hangawaththa@ifsworld.com>
2018-10-25 15:31:16 +0530

    Modifying main.py

--- web/user_crud/db_config.py
+++ web/user_crud/db_config.py
@@ -1,11 +1,12 @@
 from app import app
 from flaskext.mysql import MySQL
 
-mysql = MySQL()
+#mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'users'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+#mysql.init_app(app)
+mysql = MySQL(app)
--- web/user_crud/main.py
+++ web/user_crud/main.py
@@ -2,7 +2,7 @@ import pymysql
 from app import app
 from tables import Results
 from db_config import mysql
-from flask import flash, render_template, request, redirect
+from flask import flash, render_template, request, redirect, url_for
 from werkzeug import generate_password_hash, check_password_hash
 
 @app.route('/new_user')
@@ -12,11 +12,15 @@ def add_user_view():
 @app.route('/add', methods=['POST'])
 def add_user():
 	try:		
-		_name = request.form['inputName']
-		_email = request.form['inputEmail']
-		_password = request.form['inputPassword']
+		#_name = request.form['inputName']
+		#_email = request.form['inputEmail']
+		#_password = request.form['inputPassword']
 		# validate the received values
-		if _name and _email and _password and request.method == 'POST':
+		#if _name and _email and _password and request.method == 'POST':
+		if(request.method == 'POST'):
+			_name = request.form['inputName']
+			_email = request.form['inputEmail']
+			_password = request.form['inputPassword']
 			#do not save password as a plain text
 			_hashed_password = generate_password_hash(_password)
 			# save edits
@@ -27,7 +31,7 @@ def add_user():
 			cursor.execute(sql, data)
 			conn.commit()
 			flash('User added successfully!')
-			return redirect('/')
+			return redirect(url_for('users'))
 		else:
 			return 'Error while adding user'
 	except Exception as e:
@@ -72,13 +76,13 @@ def edit_view(id):
 @app.route('/update', methods=['POST'])
 def update_user():
 	try:		
-		_name = request.form['inputName']
-		_email = request.form['inputEmail']
-		_password = request.form['inputPassword']
-		_id = request.form['id']
 		# validate the received values
-		if _name and _email and _password and _id and request.method == 'POST':
+		if(request.method == 'POST'):
 			#do not save password as a plain text
+			_name = request.form['inputName']
+			_email = request.form['inputEmail']
+			_password = request.form['inputPassword']
+			_id = request.form['id']
 			_hashed_password = generate_password_hash(_password)
 			# save edits
 			sql = "UPDATE tbl_user SET user_name=%s, user_email=%s, user_password=%s WHERE user_id=%s"
@@ -88,7 +92,7 @@ def update_user():
 			cursor.execute(sql, data)
 			conn.commit()
 			flash('User updated successfully!')
-			return redirect('/')
+			return redirect(url_for('users'))
 		else:
 			return 'Error while updating user'
 	except Exception as e:
@@ -97,7 +101,7 @@ def update_user():
 	#	cursor.close() 
 	#	conn.close()
 		
-@app.route('/delete/<int:id>')
+@app.route('/delete/<string:id>')
 def delete_user(id):
 	try:
 		conn = mysql.connect()
@@ -105,7 +109,7 @@ def delete_user(id):
 		cursor.execute("DELETE FROM tbl_user WHERE user_id=%s", (id,))
 		conn.commit()
 		flash('User deleted successfully!')
-		return redirect('/')
+		return redirect(url_for('users'))
 	except Exception as e:
 		print(e)
 	#finally:
@@ -113,4 +117,4 @@ def delete_user(id):
 	#	conn.close()
 		
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
